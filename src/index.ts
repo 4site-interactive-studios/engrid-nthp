a9c7dec289a63bd4766e6ebe219b07b44fb1625b
Fernando Santos <fern@ndo.io>
2025-05-14 18:05:36 -0400

    Adding client font to VGS

--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,34 @@ const options: Options = {
   MaxAmount: 25000,
   MinAmountMessage: "Minimum gift amount is $5",
   MaxAmountMessage: "Maximum gift amount is $25,000",
+  VGS: {
+    "transaction.ccnumber": {
+      css: {
+        "@font-face": {
+          "font-family": "Gibson",
+          "font-style": "normal",
+          "font-weight": "400",
+          "font-display": "swap",
+          src: `url("https://acb0a5d73b67fccd4bbe-c2d8138f0ea10a18dd4c43ec3aa4240a.ssl.cf5.rackcdn.com/10028/gibson-light-webfont.woff2") format("woff2"), url("https://acb0a5d73b67fccd4bbe-c2d8138f0ea10a18dd4c43ec3aa4240a.ssl.cf5.rackcdn.com/10028/gibson-light-webfont.woff") format("woff")`,
+          "unicode-range":
+            "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD",
+        },
+      },
+    },
+    "transaction.ccvv": {
+      css: {
+        "@font-face": {
+          "font-family": "Gibson",
+          "font-style": "normal",
+          "font-weight": "400",
+          "font-display": "swap",
+          src: `url("https://acb0a5d73b67fccd4bbe-c2d8138f0ea10a18dd4c43ec3aa4240a.ssl.cf5.rackcdn.com/10028/gibson-light-webfont.woff2") format("woff2"), url("https://acb0a5d73b67fccd4bbe-c2d8138f0ea10a18dd4c43ec3aa4240a.ssl.cf5.rackcdn.com/10028/gibson-light-webfont.woff") format("woff")`,
+          "unicode-range":
+            "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD",
+        },
+      },
+    },
+  },
   onLoad: () => {
     new MembershipBenefitsModal();
     customScript(App, DonationFrequency);
