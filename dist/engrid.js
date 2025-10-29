7a6fb1d5227e1922be51c861857f9627fc0302a1
Cawe Coy Rodrigues Marega <cawecoy@gmail.com>
2025-10-28 21:27:46 -0300

    Update color of section navigation previous button

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Tuesday, October 28, 2025 @ 00:47:38 ET
+ *  Date: Tuesday, October 28, 2025 @ 20:24:45 ET
  *  By: Cawe Coy
  *  ENGrid styles: v0.22.11
  *  ENGrid scripts: v0.22.17
